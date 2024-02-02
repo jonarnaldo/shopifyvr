9a1a1d406adc0232f6aa82c3d2bf8a5796405f28
jonathan <jonarnaldo@gmail.com>
2024-02-02 03:21:38 -0800

    adds grip event listener

--- dist/components/positionTracker.js
+++ dist/components/positionTracker.js
@@ -6,18 +6,21 @@ AFRAME.registerComponent(
       this.currentPos = this.el.getAttribute('position');
       this.raycastTarget = null;
       this.currentPos = this.el.object3D.getWorldPosition(this.worldPos);
-      
-      
-      this.el.addEventListener('abuttondown', function(evt) {
-        console.log('abuttondown');
-        this.raycastTarget.setAttribute('position', this.currentPos);
-      })
-      
-      this.el.addEventListener('raycaster-intersection', function(evt) {
-        const raycastEls = evt.detail.els;        
-        this.raycastTarget = raycastEls[0];
-        console.log('raycaster interstion', this.el.object3D.getWorldPosition(this.worldPos));
-      }.bind(this));
+      this.el.addEventListener('thumbstickmoved', function (evt) {
+        if (evt.detail.y > 0.95) { console.log("DOWN"); }
+        if (evt.detail.y < -0.95) { console.log("UP"); }
+        if (evt.detail.x < -0.95) { console.log("LEFT"); }
+        if (evt.detail.x > 0.95) { console.log("RIGHT"); }
+      });
+      this.el.addEventListener("gripdown", function (evt) {
+        console.log("gripdown button pressed!");
+      });
+     
+      // this.el.addEventListener('raycaster-intersection', function(evt) {
+      //   const raycastEls = evt.detail.els;        
+      //   this.raycastTarget = raycastEls[0];
+      //   console.log('raycaster interstion', this.el.object3D.getWorldPosition(this.worldPos));
+      // }.bind(this));
     },
     update: function() {
       
